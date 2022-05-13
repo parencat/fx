290ef3f524484533ca05d9e03821d630f07c6e23
Sergey <sergey@minorodata.com>
2022-05-13 22:54:24 +0300

    create tables in migration step

---
+++ src/fx/module/database.clj
@@ -0,0 +1,12 @@
+(ns fx.module.database
+  (:require
+   [integrant.core :as ig]
+   [duct.core :as duct]))
+
+
+(defmethod ig/init-key :fx.module/database [_ _]
+  (fn [config]
+    (duct/merge-configs
+     config
+     {:fx.database/connection {}
+      :fx/migrate             {}})))
--- test/fx/migrate_test.clj
+++ test/fx/migrate_test.clj
@@ -3,19 +3,69 @@
    [clojure.test :refer :all]
    [next.jdbc :as jdbc]
    [next.jdbc.result-set :as rs]
-   [fx.containers.postgres :as pg])
-  (:import [java.sql DatabaseMetaData]))
+   [fx.containers.postgres :as pg]
+   [honey.sql :as sql])
+  (:import [java.sql DatabaseMetaData Types]))
+
+;;
+;;(def pgc
+;;  (pg/pg-container))
+;;
+;;
+;;(def con
+;;  (jdbc/get-connection
+;;   {:jdbcUrl  (.getJdbcUrl (:container pgc))
+;;    :user     (.getUsername (:container pgc))
+;;    :password (.getPassword (:container pgc))}))
+
+(require '[fx.migrate :as migrate])
+(require '[fx.entity :as entity])
+
+(def con (:fx.database/connection 'system))
+
+(def user
+  (entity/create-entity :user {:table    [:table {:name "user"}
+                                          [:id {:primary-key? true} uuid?]
+                                          [:name [:string {:max 250}]]
+                                          [:last-name {:optional? true} string?]
+                                          [:client {:many-to-one? true} :fx.entity-test/client]
+                                          [:role {:many-to-one? true} :fx.entity-test/role]]
+                               :database con}))
 
 
-(def pgc
-  (pg/pg-container))
+(def entities
+  (-> (select-keys {} [[:fx/entity :fx.entity-test/role]
+                       [:fx/entity :fx.entity-test/user]
+                       [:fx/entity :fx.entity-test/client]])
+      vals
+      set))
 
+(migrate/extract-db-columns con entities)
+(migrate/get-table-columns con "user")
+(migrate/db->simple-schema (migrate/extract-db-columns con entities))
+(migrate/entities->simple-schema @entity/entities-lookup entities)
 
-(def con
-  (jdbc/get-connection
-   {:jdbcUrl  (.getJdbcUrl (:container pgc))
-    :user     (.getUsername (:container pgc))
-    :password (.getPassword (:container pgc))}))
+(data/diff (migrate/db->simple-schema (migrate/extract-db-columns con entities))
+           (migrate/entities->simple-schema @entity/entities-lookup entities))
+
+(data/diff {"role" {"id" {:type "uuid", :nullable false, :primary-key true},
+                    "name" {:type "varchar", :nullable false},
+                    "test-name" {:type "varchar", :nullable true}},
+            "user" {"id" {:type "uuid", :nullable false, :primary-key true},
+                    "name" {:type "varchar", :nullable false},
+                    "last-name" {:type "varchar", :nullable true},
+                    "client" {:type "uuid", :nullable false, :foreign-key true, :references "client"},
+                    "role" {:type "uuid", :nullable false, :foreign-key true, :references "role"}}}
+
+           {"role" {"id" {:type "uuid", :nullable false, :primary-key true},
+                    "name" {:type "varchar", :nullable false},
+                    "test-name" {:type "varchar", :nullable true}},
+            "client" {"id" {:type "uuid", :nullable false, :primary-key true}, "name" {:type "varchar", :nullable false}},
+            "user" {"id" {:type "uuid", :nullable false, :primary-key true},
+                    "name" {:type "varchar", :nullable false},
+                    "last-name" {:type "varchar", :nullable true},
+                    "client" {:type "uuid", :nullable false, :foreign-key true, :references "client"},
+                    "role" {:type "uuid", :nullable false, :foreign-key true, :references "role"}}})
 
 
 (def tables-query
@@ -37,6 +87,7 @@
 
 
 (jdbc/execute! con [tables-query])
+(jdbc/execute! con ["alter table role add column test_name varchar (250);"])
 
 
 (-> ^DatabaseMetaData (.getMetaData con)
@@ -46,25 +97,54 @@
 
 
 (-> ^DatabaseMetaData (.getMetaData con)
-    (.getColumns nil "public" nil nil)
-    (rs/datafiable-result-set con {:builder-fn rs/as-unqualified-kebab-maps})
-    ((partial map #(select-keys % [:column-name :table-name :ordinal-position :type-name])))
-    ((partial group-by :table-name)))
+    (.getColumns nil "public" "user" nil)
+    (rs/datafiable-result-set con {:builder-fn rs/as-unqualified-kebab-maps}))
+
+(-> ^DatabaseMetaData (.getMetaData con)
+    (.getPrimaryKeys nil nil "user")
+    (rs/datafiable-result-set con {:builder-fn rs/as-unqualified-kebab-maps}))
+
+(-> ^DatabaseMetaData (.getMetaData con)
+    (.getImportedKeys nil nil "user")
+    (rs/datafiable-result-set con {:builder-fn rs/as-unqualified-kebab-maps}))
+
 
 
 ;; from DB
-{"client" [{:column-name "id", :table-name "client", :ordinal-position 1, :type-name "uuid"}
-           {:column-name "name", :table-name "client", :ordinal-position 2, :type-name "varchar(250)"}],
- "role"   [{:column-name "id", :table-name "role", :ordinal-position 1, :type-name "uuid"}
-           {:column-name "name", :table-name "role", :ordinal-position 2, :type-name "varchar"}],
- "user"   [{:column-name "id", :table-name "user", :ordinal-position 1, :type-name "uuid"}
-           {:column-name "name", :table-name "user", :ordinal-position 2, :type-name "varchar"}
-           {:column-name "last_name", :table-name "user", :ordinal-position 3, :type-name "varchar"}
-           {:column-name "client", :table-name "user", :ordinal-position 4, :type-name "uuid"}
-           {:column-name "role", :table-name "user", :ordinal-position 5, :type-name "uuid"}]}
+(def sample
+  {"client" {"id"   {:type "uuid"}
+             "name" {:type "varchar(250)"}}
+
+   "role"   {"id",  {:type "uuid"}
+             "name" {:type "varchar"}}
+
+   "user"   {"id"        {:type "uuid"}
+             "name"      {:type "varchar"}
+             "last_name" {:type "varchar"}
+             "client"    {:type "uuid"}
+             "role"      {:type "uuid"}}})
 
 
 (require '[clojure.data :as data])
+
+(data/diff sample
+           {"client" {"id"   {:type "uuid"}
+                      "name" {:type "varchar(30)"}}
+
+            "user"   {"id"     {:type "uuid"}
+                      "name"   {:type "varchar"}
+                      "client" {:type "uuid"}
+                      "role"   {:type "uuid"}}})
+
+
+({"user"   {"last_name" {:type "varchar"}},
+  "client" {"name" {:type "varchar(250)"}},
+  "role"   {"id" {:type "uuid"}, "name" {:type "varchar"}}}
+
+ {"client" {"name" {:type "varchar(30)"}}}
+
+ {"user"   {"role" {:type "uuid"}, "client" {:type "uuid"}, "name" {:type "varchar"}, "id" {:type "uuid"}},
+  "client" {"id" {:type "uuid"}}})
 
 
 ;; schemas lookup
@@ -115,6 +195,14 @@
   [:last-name :varchar]
   [:client :uuid [:not nil] [:references :client :id]]
   [:role :uuid [:not nil] [:references :role]]]}
+
+(sql/format {:create-table "user"
+             :with-columns
+             [[:id :uuid [:not nil] [:primary-key]]
+              [:name :varchar [:not nil]]
+              [:last-name :varchar]
+              [:client :uuid [:not nil] [:references :client]]
+              [:role :uuid [:not nil] [:references :role]]]})
 
 {:alter-table :fruit
  :add-column  [:id :int [:not nil]]}
