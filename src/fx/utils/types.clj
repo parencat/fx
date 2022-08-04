24622caa676484ffda31ed6a6f6adb6bb9c6acd2
Sergey <sergey@minorodata.com>
2022-08-04 17:04:36 +0300

    more options for storing migration files

--- src/fx/utils/types.clj
+++ src/fx/utils/types.clj
@@ -2,10 +2,17 @@
   (:require
    [malli.core :as m])
   (:import
-   [java.sql Connection]))
+   [java.sql Connection]
+   [java.time Clock]))
 
 
 (def connection?
   (m/-simple-schema
    {:type :db/connection
     :pred #(instance? Connection %)}))
+
+
+(def clock?
+  (m/-simple-schema
+   {:type :java.time/clock
+    :pred #(instance? Clock %)}))
