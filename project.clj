351145cf9817e693c352656b9c0a57b6183f0a3c
Sergey <sergey@minorodata.com>
2022-09-25 23:36:59 +0300

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.parencat/fx "0.1.2"
+(defproject io.github.parencat/fx "0.1.3-SNAPSHOT"
   :description "Set of Duct modules for rapid clojure development"
   :url "https://github.com/parencat/fx"
 
