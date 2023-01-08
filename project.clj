359bb8d615c98c299e3c6202ab7e71764edf9b9a
Sergey <sergey@minorodata.com>
2023-01-09 00:57:02 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.namespace "1.3.0"]
                  [org.clojure/java.classpath "1.0.0"]
-                 [org.clojure/core.match "1.0.0"]
+                 [org.clojure/core.match "1.0.1"]
                  [duct/core "0.8.0"]
                  [integrant "0.8.0"]
                  [weavejester/dependency "0.2.1"]
@@ -16,16 +16,16 @@
                  [differ "0.3.3"]
                  [com.github.seancorfield/next.jdbc "1.3.847"]
                  [com.github.seancorfield/honeysql "2.4.947"]
-                 [org.postgresql/postgresql "42.5.0"]
-                 [hikari-cp "3.0.0"]
+                 [org.postgresql/postgresql "42.5.1"]
+                 [hikari-cp "3.0.1"]
                  [clj-test-containers "0.7.4"]
                  [org.testcontainers/postgresql "1.17.6"]
-                 [com.cnuernber/charred "1.014"]]
+                 [com.cnuernber/charred "1.018"]]
 
   :profiles
   {:dev {:source-paths ["dev"]
          :dependencies [[vvvvalvalval/scope-capture "0.3.3-s1"]
-                        [djblue/portal "0.34.2"]]}}
+                        [djblue/portal "0.35.1"]]}}
 
   :deploy-repositories
   [["clojars" {:sign-releases false
